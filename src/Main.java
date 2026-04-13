5871eace68e95bc0a3eec5b0409ff340c522f9c9
Jonas <jonas.thai@live.dk>
2026-04-13 13:35:06 +0200

    refactor: add variable to Main.main()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello Git"
+        String message = "Hello Git";
 
         System.out.println(message);
     }
